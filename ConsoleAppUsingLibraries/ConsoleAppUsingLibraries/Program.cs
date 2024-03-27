5f82dd5340b075e85d58a10a023234203795a149
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2024-03-27 18:02:18 +0100

    Add files via upload

---
+++ ConsoleAppUsingLibraries/ConsoleAppUsingLibraries/Program.cs
@@ -0,0 +1,73 @@
+﻿using RegexUtils;
+
+namespace ConsoleAppUsingLibraries
+{
+    internal class Program
+    {
+        static int sceltaUtente;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                SceltaUtente();
+                do
+                {
+                    switch (sceltaUtente)
+                    {
+                        case 1:
+                            Console.WriteLine("Inserisci un indirizzo email");
+                            String email = Console.ReadLine();
+                            Console.WriteLine("L'indirizzo email è {0}", RegexUtils.Class1.IsValidEmail(email) ? "valido" : "non valido\n");
+                            SceltaUtente();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Inserisci nome e cognome in questo formato:\n" +
+                                "Nome: John Cognome: Doe");
+                            String userInput = Console.ReadLine();
+                            (String nome, String cognome) = RegexUtils.Class1.GetPersonalData(userInput);
+                            Console.WriteLine($"Nome: {nome}, Cognome: {cognome}\n");
+                            SceltaUtente();
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Inserisci una frase con la tua altezza, o solo la tua altezza, in metri, compresa di punto o virgola");
+                            String altezza = Console.ReadLine();
+                            Console.WriteLine(RegexUtils.Class1.HeightConversion(altezza) + "\n");
+                            SceltaUtente();
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Scrivi una frase");
+                            String frase = Console.ReadLine();
+                            Console.WriteLine("Digita la vocale che vuoi prenda il sopravvento");
+                            char vocale = Convert.ToChar(Console.ReadLine());
+                            Console.WriteLine(RegexUtils.Class1.MonoVocalizzazione(frase, vocale) + "\n");
+                            SceltaUtente();
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                while (sceltaUtente != 9);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static public void SceltaUtente()
+        {
+            Console.WriteLine("Seleziona l'operazione da eseguire\n" +
+                "1.Validazione indirizzo email\n" +
+                "2.Estrazione nome e cognome\n" +
+                "3.Conversione altezza da metri a cm\n" +
+                "4.Conversione vocali\n" +
+                "9.Esci dal programma");
+            sceltaUtente = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ RegexUtils/RegexUtils/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexUtils
+{
+    public class Class1
+    {
+        private static readonly Regex EmailRegex = new Regex(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", RegexOptions.Compiled);
+
+        private static readonly Regex AnagraficaRegex = new Regex(@"Nome: (?<nome>\w+) Cognome: (?<cognome>\w+)", RegexOptions.Compiled);
+
+        private static readonly Regex AltezzaRegex = new Regex(@"[.,]", RegexOptions.Compiled);
+
+        private static readonly Regex MonoVocalizzazioneARegex = new Regex(@"[eiouEIOU]");
+        private static readonly Regex MonoVocalizzazioneERegex = new Regex(@"[aiouAIOU]");
+        private static readonly Regex MonoVocalizzazioneIRegex = new Regex(@"[aeouAEOU]");
+        private static readonly Regex MonoVocalizzazioneORegex = new Regex(@"[aeiuAEIU]");
+        private static readonly Regex MonoVocalizzazioneURegex = new Regex(@"[aeioAEIO]");
+
+        //Validazione email
+        public static bool IsValidEmail(String email)
+        {
+            return EmailRegex.IsMatch(email);
+        }
+
+        //Estrazione nome e cognome
+        public static (String, String) GetPersonalData(String input)
+        {
+            Match match = AnagraficaRegex.Match(input);
+
+            if (match.Success)
+            {
+                String nome = match.Groups["nome"].Value;
+                String cognome = match.Groups["cognome"].Value;
+
+                return (nome, cognome);
+            }
+            else
+            {
+                return (String.Empty, String.Empty);
+            }
+        }
+
+        //Conversione altezza da metri a centimetri
+        public static String HeightConversion(String input)
+        {
+            String altezzaConvertita = AltezzaRegex.Replace(input, "");
+            String nuovaAltezza;
+            return nuovaAltezza = (altezzaConvertita + "cm");
+        }
+
+        //Sostituzione di tutte le vocali con una a scelta
+        public static String MonoVocalizzazione(String input, char c)
+        {
+            char c2 = char.ToLower(c);
+
+            if (c2 == 'a')
+            {
+                return MonoVocalizzazioneARegex.Replace(input, "a");
+            }
+            else if (c2 == 'e')
+            {
+                return MonoVocalizzazioneERegex.Replace(input, "e");
+            }
+            else if (c2 == 'i')
+            {
+                return MonoVocalizzazioneIRegex.Replace(input, "i");
+            }
+            else if (c2 == 'o')
+            {
+                return MonoVocalizzazioneORegex.Replace(input, "o");
+            }
+            else if (c2 == 'u')
+            {
+                return MonoVocalizzazioneURegex.Replace(input, "u");
+            }
+            return "";
+        }
+    }
+}
